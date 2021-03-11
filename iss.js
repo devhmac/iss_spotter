33aad6cb0009c90a5c25d1eef515a7513efc9070
Devin MacGillivray <devhmac@gmail.com>
2021-03-11 22:27:27 +0000

    added comment

--- iss.js
+++ iss.js
@@ -9,7 +9,7 @@ const request = require("request");
 
 // *   - The IP address as a string (null if error). Example: "162.245.144.188"
 const fetchMyIp = (callback) => {
-
+  //request to ipify api
   request('https://api.ipify.org?format=json', (error, response, body) => {
     //if error, pass it into callback
     if (error) return callback(error);
