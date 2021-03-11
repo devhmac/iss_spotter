5e225952791e1ebdd01ee1f29b116396fde09ce4
Devin MacGillivray <devhmac@gmail.com>
2021-03-11 22:14:37 +0000

    setting up fetchMyIp in iss.js and calling with callback to output result in index

---
+++ index.js
@@ -0,0 +1,11 @@
+const { fetchMyIp } = require('./iss');
+
+fetchMyIp((error, ip) => {
+  if (error) {
+    console.log('There was an Error: ', error);
+    return;
+  }
+  console.log('It worked! IP returned', ip)
+
+
+});
