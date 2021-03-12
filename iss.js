2de5f4f5fb79633c26339912b214bd28ce2c2242
Devin MacGillivray <devhmac@gmail.com>
2021-03-12 01:15:17 +0000

    linted iss.js

--- iss.js
+++ iss.js
@@ -41,6 +41,7 @@ const fetchCoordsByIP = (ip, callback) => {
 
 const fetchISSFlyOverTimes = (coords, callback) => {
   request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
+
     if (error) return callback(error, null);
     const flyOverReturn = JSON.parse(body);
     if (response.statusCode !== 200) {
