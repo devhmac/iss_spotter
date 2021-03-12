6d26e374f96cd3b052ca38ae1e46336954706950
Devin MacGillivray <devhmac@gmail.com>
2021-03-12 01:11:28 +0000

    looped though passTime array converting utc time to datetime

--- index.js
+++ index.js
@@ -1,40 +1,18 @@
-const { fetchMyIp } = require('./iss');
-const { fetchCoordsByIP } = require('./iss');
-const { fetchISSFlyOverTimes } = require('./iss');
 const { nextISSTimesForMyLocation } = require('./iss')
+
+const convertPassTimes = (passTime) => {
+  for (const pass of passTime) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime)
+    console.log(`Next pass on ${datetime} for ${pass.duration} seconds!`)
+  }
+};
+
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log('It didnt work: ', error);
   }
-  const flyOverArray = passTimes
-  for (let pass of flyOverArray) {
-    console.log(pass)
-  }
-})
+  convertPassTimes(passTimes)
+});
 
-// fetchMyIp((error, ip) => {
-//   if (error) {
-//     console.log('There was an Error: ', error);
-//     return;
-//   }
-//   console.log('It worked! IP returned', ip);
-
-// });
-
-// fetchCoordsByIP('96.52.15.154', (error, data) => {
-//   if (error) {
-//     console.log('There was an Error: ', error);
-//     return;
-//   }
-//   console.log('It worked here are the coordinates: ', data);
-
-// });
-
-// fetchISSFlyOverTimes({ latitude: 53.5185, longitude: -113.6579 }, (error, flyOverData) => {
-//   if (error) {
-//     console.log('There was an Error: ', error);
-//     return;
-//   }
-//   console.log('It worked here are the flyOvers: ', flyOverData);
-// });
