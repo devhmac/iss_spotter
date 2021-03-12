28cba097e24afe6d50cc82dbc10fdea34a6f7715
Devin MacGillivray <devhmac@gmail.com>
2021-03-12 03:00:30 +0000

    linted index2 & iss promised

--- index2.js
+++ index2.js
@@ -1,10 +1,17 @@
 const { nextISSTimesForMyLocation } = require('./iss_promised');
-const { convertPassTimes } = require('./index')
+
+const convertPassTimes = (passTime) => {
+  for (const pass of passTime) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    console.log(`Next pass on ${datetime} for ${pass.duration} seconds!`);
+  }
+};
 
 nextISSTimesForMyLocation()
   .then((passTimesArray) => {
     convertPassTimes(passTimesArray);
   })
   .catch((error) => {
-    console.log("It Did not work: ", error.message)
+    console.log("It Did not work: ", error.message);
   });
--- iss_promised.js
+++ iss_promised.js
@@ -12,17 +12,17 @@ const fetchCoordsByIP = (data) => { //this will take in json data for an ip
 const fetchISSFlyOverTimes = (data) => { //takes in coordinate json data
   const coords = JSON.parse(data);
   return request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`);
-}
-// we have made a function that can execute the promise chain here, which we'll export to index, and call there. 
+};
+// we have made a function that can execute the promise chain here, which we'll export to index, and call there.
 const nextISSTimesForMyLocation = () => {
   return fetchMyIP()
     //.then((ip) => { console.log(ip); })
     .then(fetchCoordsByIP)
     .then(fetchISSFlyOverTimes)
     .then((data) => {
-      const { response } = JSON.parse(data) // this is pulling the .response section  (which has the array of flyover times) from the json string
+      const { response } = JSON.parse(data); // this is pulling the .response section  (which has the array of flyover times) from the json string
       return response;
-    })
+    });
 };
 
 module.exports = { nextISSTimesForMyLocation };
